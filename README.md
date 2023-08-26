e170c12774f270fb236e1930d3a5af1f3a809e9e
peter mbugua <petermbuzi84@gmail.com>
2023-08-26 17:41:52 +0300

    putting more details on how the code should be used

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # mathstudiokotlin
 MathStudioKotlin has classes and functions that makes mathematical calculations based on formulas, methods and other mathematical algorithms.
+
+use the functions to provide an answer for the following question:
+
+*A minor arc of a circle subtends an angle of 105 degrees at the center of the circle. If the radius of the circle is 8.4 cm, find the length of the major arc.*
+
+The solution to the problem above is 37.4 cm
+
+### Tests
+There are test functions that confirms that the math functions are working properly.
+If you decide to alter the function to your needs, you can run the tests to ensure that they are working as expected.
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,19 @@
-fun measurementQuestion3and4() {
+fun measurementQuestion() {
     val angle = 105.0
     val radius = 8.4
-    println()
+    /* use the functions to provide an answer for the following question
+    * A minor arc of a circle subtends an angle of 105 degrees at the center
+    * of the circle. If the radius of the circle is 8.4 cm, find the length
+    * of the major arc.
+    * */
+
+    // put your code here
+
+    println() // print the answer here = the solution is 37.4 cm
 }
 
 fun main() {
     println("===========================MathStudio===========================")
-    measurementQuestion3and4()
+    measurementQuestion()
     println("================================================================")
 }
